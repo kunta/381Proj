db16ab2d01a74ad7bfa2a4c58106c15640d904b1
Diacre <bdiacre@yahoo.com>
2012-11-15 16:04:31 -0600

    added keyboard.xml

--- Project381/gen/com/example/project381/R.java
+++ Project381/gen/com/example/project381/R.java
@@ -21,37 +21,40 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int delete=0x7f07000a;
-        public static final int editText1=0x7f07000e;
-        public static final int file=0x7f070004;
-        public static final int help=0x7f07000d;
-        public static final int lineNumbers=0x7f070001;
-        public static final int main_text_input=0x7f070002;
-        public static final int menu_settings=0x7f070003;
-        public static final int new_file=0x7f070005;
-        public static final int numbers=0x7f07000c;
-        public static final int open=0x7f070006;
-        public static final int save=0x7f070007;
-        public static final int save_all=0x7f070009;
-        public static final int save_as=0x7f070008;
-        public static final int tabhost=0x7f070000;
-        public static final int view=0x7f07000b;
+        public static final int delete=0x7f08000a;
+        public static final int editText1=0x7f08000e;
+        public static final int file=0x7f080004;
+        public static final int help=0x7f08000d;
+        public static final int lineNumbers=0x7f080001;
+        public static final int main_text_input=0x7f080002;
+        public static final int menu_settings=0x7f080003;
+        public static final int new_file=0x7f080005;
+        public static final int numbers=0x7f08000c;
+        public static final int open=0x7f080006;
+        public static final int save=0x7f080007;
+        public static final int save_all=0x7f080009;
+        public static final int save_as=0x7f080008;
+        public static final int tabhost=0x7f080000;
+        public static final int view=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
-        public static final int menu=0x7f060001;
-        public static final int new_file=0x7f060002;
+        public static final int activity_main=0x7f070000;
+        public static final int menu=0x7f070001;
+        public static final int new_file=0x7f070002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int keyboard=0x7f040000;
     }
 }
